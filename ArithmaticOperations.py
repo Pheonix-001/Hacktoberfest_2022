00cc13bfda7a9daf4941e610c9832d4b4b1bc41a
Doremon <www.saylipatil1603@gmail.com>
2022-10-15 15:19:03 +0530

    Arithmatic Opetations in python

---
+++ ArithmaticOperations.py
@@ -0,0 +1,27 @@
+"""
+different arithmatic operation in python
+"""
+
+# take input from user
+num1 = int(input('enter the value of num1: '))
+num2 = int(input('enter the value of num2: '))
+
+# adding two numbers
+add = num1 + num2
+
+# subtracting two numbers
+sub = num1 - num2
+
+# multiplication of two numbers
+mul = num1 * num2
+
+# division of two numbers
+div = num1 // num2
+
+
+print('addition of numbers is: ',add)
+print('subtraction of numbers is: ',sub)
+print('multiplication of numbers is: ',mul)
+print('division of numbers is: ',div)
+
+
